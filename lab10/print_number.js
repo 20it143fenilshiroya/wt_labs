931d41b8118e75da11ed12719123dde6d8bcc985
20it143fenilshiroya <88379013+20it143fenilshiroya@users.noreply.github.com>
2022-03-01 14:07:46 +0530

    Add files via upload

---
+++ lab10/print_number.js
@@ -0,0 +1,16 @@
+function odd_even(){
+
+    let x=prompt("enter any number");
+    x=parseInt(x);
+    var i;
+    i=parseInt(i);
+    let t="";
+    x=parseInt(x);
+    
+    for(i=1;i<=x;i++)
+    {
+        t+=i+"<br>";
+    }
+    document.getElementById('n1').innerHTML=t;
+  
+}
